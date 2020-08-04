6cc52e183c0fe957bb69f43e9619d4eae1342512
FIC <FIC@info.local>
2020-08-04 00:19:19 -0300

    criada web landing page responsiva

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from "./Pages/Landing"
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
